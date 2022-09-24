3031e3db4c4dd3a2359262728e9992d379051c4d
abdoulfataoh <abdoulfataoh@gmail.com>
2022-09-24 04:11:50 +0000

    add doc

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+from pydoc import describe
 import streamlit as st
 from PIL import Image
 import altair as alt
@@ -28,54 +29,71 @@ st.markdown(
 # sidebar
 st.sidebar.header('Features')
 feature = st.sidebar.selectbox(
-    'Dataset Exploaration',
-    ['Statistic', 'Sample Example', '']
+    'select feature',
+    ['describe dataset', 'show dataset distribution', 'explore dataset']
 )
 
-st.markdown(
-    """
-    #### Description
-    """
-)
-describe = list(sc.describe_dataset().items())
-col1, col2, col3 = st.columns(3)
-col1.metric(describe[0][0], describe[0][1])
-col2.metric(describe[1][0], describe[1][1])
-col3.metric(describe[2][0], describe[2][1])
 
-col1, col2, col3 = st.columns(3)
-col1.metric(describe[3][0], describe[3][1])
-col2.metric(describe[4][0], describe[4][1])
-col3.metric(describe[5][0], describe[5][1])
 
-st.markdown(
-    """
-    #### Distribution des commentaires par article
-    """
-)
+def description():
+    st.markdown(
+        """
+        #### Description
+        """
+    )
+    describe = list(sc.describe_dataset().items())
+    col1, col2, col3 = st.columns(3)
+    col1.metric(describe[0][0], describe[0][1])
+    col2.metric(describe[1][0], describe[1][1])
+    col3.metric(describe[2][0], describe[2][1])
 
-# distrubtion
-source = sc.get_dataset()['comments_number']
-st.bar_chart(source)
+    col1, col2, col3 = st.columns(3)
+    col1.metric(describe[3][0], describe[3][1])
+    col2.metric(describe[4][0], describe[4][1])
+    col3.metric(describe[5][0], describe[5][1])
 
-# show json
-st.markdown(
-    """
-    #### Visualisation contenu
-    """
-)
-samples = sc.get_samples()
-articles = {}
-i = 0
-for sample in samples:
-    i = i + 1
-    title = sample['article_title']
-    key = f'{i} - {title}'
-    value = sample
-    articles[key] = value
+def comments_distribution():
+    st.markdown(
+        """
+        #### Distribution des commentaires par article
+        """
+    )
 
-article_select = st.selectbox(
-    'choisir un article',
-    list(articles.keys())
-)
-st.json(articles.get(article_select))
+    # distrubtion
+    source = sc.get_dataset()['comments_number']
+    st.bar_chart(source)
+
+
+def explore_dataset():                 
+    # show json
+    st.markdown(
+        """
+        #### Visualisation contenu
+        """
+    )
+    samples = sc.get_samples()
+    articles = {}
+    i = 0
+    for sample in samples:
+        i = i + 1
+        title = sample['article_title']
+        key = f'{i} - {title}'
+        value = sample
+        articles[key] = value
+
+    article_select = st.selectbox(
+        'choisir un article',
+        list(articles.keys())
+    )
+    st.json(articles.get(article_select))
+
+
+#### RUN
+call = {
+    'describe dataset': description,
+    'show dataset distribution': comments_distribution,
+    'explore dataset': explore_dataset,
+}
+
+call.get(feature)()
+
