4305f6609d3d443d1eca396b6835cb4c873cfecc
abdoul fataoh kabore <32961108+abdoulfataoh@users.noreply.github.com>
2023-08-20 17:49:58 +0000

    update start.py

--- start.py
+++ start.py
@@ -16,16 +16,15 @@ comments_by_article_date = extract_comments_by_article_date(
     settings.DATASET_PATH
 )
 
-# an_comments_by_article_date = comments_sentiment_prediction(
-#     comments=comments_by_article_date,
-#     model=flaubert_large_cased
-# )
+an_comments_by_article_date = comments_sentiment_prediction(
+    comments=comments_by_article_date,
+    model=flaubert_large_cased
+)
 
 an_comments_by_article_date = hate_comments_prediction(
     comments=comments_by_article_date,
     model=flaubert_large_cased
 )
 
-
-# with open('an_comments_by_article_date.pickle', 'wb') as file:
-#     pickle.dump(an_comments_by_article_date, file)
+with open('an_comments_by_article_date.pickle', 'wb') as file:
+    pickle.dump(an_comments_by_article_date, file)
